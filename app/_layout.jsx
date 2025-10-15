22fa4a35bf71b6f6a9e9c6e5a3651b85b0299e22
lionsgrandson <mosheschwartzberg@gmail.com>
2025-10-15 18:22:20 +0300

    style: basic styling

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,24 @@
-import { Stack } from "expo-router";
-
+import { Stack } from 'expo-router'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { Appearance } from 'react-native'
+import { Colors } from '@/constant/theme'
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = Appearance.getColorScheme()
+  const theme = colorScheme === 'dark' ? Colors.dark : Colors.light
+  return (
+    <SafeAreaProvider
+      screenOptions={{
+        headerStyle: { backgroundColor: theme.headerBackground },
+        headerTintColor: theme.text,
+        headerShadowVisible: false,
+      }}
+    >
+      <Stack>
+        <Stack.Screen
+          name='index'
+          options={{ title: 'Home', headerShown: true }}
+        />
+      </Stack>
+    </SafeAreaProvider>
+  )
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
-
+import { Text, View, Appearance, StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Colors } from '@/constant/theme'
 export default function Index() {
+  const colorScheme = Appearance.getColorScheme()
+  const theme = colorScheme === 'dark' ? Colors.dark : Colors.light
+  const styles = createStyles(theme)
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    <SafeAreaView style={styles.view}>
+      <View>
+        <Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+function createStyles(theme) {
+  return StyleSheet.create({
+    view: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: theme.background,
+    },
+    text: {
+      color: theme.text,
+      fontSize: 20,
+      marginBottom: 10,
+    },
+  })
 }
